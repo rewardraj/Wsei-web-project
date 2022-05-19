8769334a82ec61a1bae582f4ea997012401c7850
Reward Edoyugbo <rewardraj@gmail.com>
2022-05-19 21:05:48 +0200

    added review section, and footer

---
+++ js/init.js
@@ -0,0 +1,5 @@
+var clickLeft = document.querySelector(".carousel-control-prev");
+var clickRight = document.querySelector(".carousel-control-next");
+
+clickLeft.addEventListener("click", clickLeft);
+clickRight.addEventListener("click", clickRight);
