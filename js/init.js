dd5b78475e4199fe4d21c900e95ccc0915b65b12
Reward Edoyugbo <rewardraj@gmail.com>
2022-06-04 02:05:14 +0200

    added 2 new sections and footer, added logo

--- js/init.js
+++ js/init.js
@@ -34,3 +34,76 @@
 })();
 
 /* Owl carousel end*/
+
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+const gradients = [
+  "linear-gradient(to right top, #f46b45, #eea849)",
+  "linear-gradient(to right top, #005c97, #363795)",
+  "linear-gradient(to right top, #e53935, #e35d5b)",
+];
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries){
+  entries.forEach(entry => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const gradientIndex = entry.target.getAttribute('data-index');
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left
+    };
+    if(entry.isIntersecting){
+      bubble.style.setProperty("left", `${directions.left}px`);
+      bubble.style.setProperty("top", `${directions.top}px`);
+      bubble.style.setProperty("width", `${directions.width}px`);
+      bubble.style.setProperty("height", `${directions.height}px`);
+      bubble.style.background = gradients[gradientIndex];
+    }
+  });
+}
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+
+// Nav slide js
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  
+  
+  burger.addEventListener('click', () => {
+    //toggle nav
+    nav.classList.toggle('nav-active');
+
+    
+  //animate links
+  navLinks.forEach((link, index) => {
+    if(link.style.animation){
+      link.style.animation = '';
+    }
+    else{
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+    });
+
+    //Burger Animation
+    burger.classList.toggle('toggle');
+
+
+  });
+
+}
+
+navSlide();
